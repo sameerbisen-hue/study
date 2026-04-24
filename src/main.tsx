503a0bf1237a122fe5e93ae1234698db2ee1e3de
sameerbisen-hue <sameerbisen@acpce.ac.in>
2026-04-24 22:46:51 +0530

    Fix reload issue when revisiting website - add NavigationStateManager

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { StoreProvider } from "./services/store";
+import { NavigationStateManager } from "./components/NavigationStateManager";
 
 createRoot(document.getElementById("root")!).render(
   <StoreProvider>
-    <App />
+    <NavigationStateManager>
+      <App />
+    </NavigationStateManager>
   </StoreProvider>
 );
